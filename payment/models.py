26a904a1a711bd5a0363048cbd317ea7f369f59b
estherlaws <estherelaws@gmail.com>
2024-05-12 05:11:07 -0400

    Add Shipping Automation to Admin

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from store.models import Product
+import datetime
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -35,10 +37,22 @@ class Order(models.Model):
     shipping_address = models.TextField(max_length=500)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
+    date_shipped = models.DateTimeField(blank=True, null=True)
     shipped = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Order - {str(self.id)}"
+    
+@receiver(pre_save, sender=Order)
+
+def update_shipped_date(sender, instance, **kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
+
 
 class OrderItems(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
